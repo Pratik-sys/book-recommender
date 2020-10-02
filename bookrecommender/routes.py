a7a29bd0b2ca270862b687ba05fcf592bfee237c
Pratik-Sys <pithore.pratik@gmail.com>
2020-10-02 08:07:23 +0530

    modfiy the test route
    - For instance the user now can search through "tags" (from csv) and the table will be populated according to the search value
    
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for, request
 import pandas as pd 
 import numpy as np
 import os 
+from bookrecommender.forms import GetField
 
 @app.route('/home', methods=['POST', 'GET'])
 @app.route('/')
@@ -12,12 +13,15 @@ def home():
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
+    form = GetField(request.form)
     data_read = pd.read_csv("/Users/bug/book-recommendation/bookrecommender/new_dataset.csv")
     # data_sort = data_read.sort_values(by=['rating-avg'],ascending=False).head(n=5)
-    p = pd.DataFrame(['AI'],columns=['Tags'])
+    value = form.title.data
+    print(value)
+    p = pd.DataFrame([value],columns=['Tags'])
     x = pd.merge(p,data_read.sort_values(by=['rating-avg'], ascending=False))
-    h = x.to_html()
-    return render_template("test.html", h=h)
+    xc = pd.DataFrame(x)
+    return render_template("test.html", xc=xc.to_html(), form=form )
 
 @app.route('/display',methods=['POST', 'GET'])
 def display():
