a1be96be9d25edcd39df9f1a5a2be773289f5cfc
Pratik-Sys <pithore.pratik@gmail.com>
2020-10-04 23:38:53 +0530

    refacor the code
    - change the variable name
    - used flask_table for custom table formation
    
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -1,30 +1,51 @@
 from bookrecommender import app
+from flask_table import Table, Col
 from flask import render_template, url_for, request
 import pandas as pd
 import numpy as np
 import os
 from bookrecommender.forms import GetField
+# from IPython.display import Image, HTML
 
+
+class BookTable(Table):
+    image = Col('Image')
+    Title = Col('Title')
+    Edition = Col('Edition')
+    Publishers = Col('Publishers')
+    Tags = Col('Tags')
+
+
+# def path_to_image_html(path):
+#     return '<img src="'+ path + '" style=max-height:124px;"/>'
 
 @app.route("/home", methods=["POST", "GET"])
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
-@app.route("/test", methods=["POST", "GET"])
-def test():
-    form = GetField(request.form)
-    data_read = pd.read_csv("./bookrecommender/dataset.csv")
-    # data_sort = data_read.sort_values(by=['rating-avg'],ascending=False).head(n=5)
-    value = form.title.data
-    print(value)
-    p = pd.DataFrame([value], columns=["Tags"])
-    x = pd.merge(p, data_read.sort_values(by=["rating-avg"], ascending=False))
-    xc = pd.DataFrame(x)
-    return render_template("test.html", xc=xc.to_html(), form=form, header=True)
-
-
 @app.route("/display", methods=["POST", "GET"])
 def display():
     return render_template("display.html")
+
+@app.route("/recommend", methods=["POST", "GET"])
+def recommend():
+
+    form = GetField(request.form)
+    data_read = pd.read_csv("./bookrecommender/MD.csv")    
+    sort_books = pd.DataFrame([form.title.data], columns=["Tags"])
+    merge_books = pd.merge(sort_books, data_read.sort_values(by=["rating-avg"], ascending=False))
+    df = pd.DataFrame(merge_books)
+    output_dict = df.to_dict(orient='records') 
+    table = BookTable(output_dict) 
+    return render_template("test.html", df=table.__html__(), form=form)
+
+
+
+
+# @app.route("/ok", methods=["POST", "GET"])
+# def ok():
+#     data_read = pd.read_csv("./bookrecommender/test2.csv")
+#     df = pd.DataFrame(data_read)
+
+#     return render_template("ok.html", df=df.to_html(classes='table', escape=False))
