84e2cc0dab2021e46a98986ef09a4764fae9e87a
Pratik-Sys <pithore.pratik@gmail.com>
2020-10-05 14:50:11 +0530

    remove redundant routes
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -1,39 +1,13 @@
 from bookrecommender import app
-from flask_table import Table, Col
 from flask import render_template, url_for, request
 import pandas as pd
 import numpy as np
-import os
 from bookrecommender.forms import GetField
-
-# from IPython.display import Image, HTML
-
-
-class BookTable(Table):
-    image = Col("Image")
-    # Title = Col('Title')
-    # Edition = Col('Edition')
-    # Publishers = Col('Publishers')
-    # Tags = Col('Tags')
-
-
-#     def make_image(self,image):
-#         return '<img src="{}"/>'.format(self.image)
 
 
 @app.route("/home", methods=["POST", "GET"])
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("home.html")
-
-
-@app.route("/display", methods=["POST", "GET"])
-def display():
-    return render_template("display.html")
-
-
-@app.route("/recommend", methods=["POST", "GET"])
-def recommend():
 
     form = GetField(request.form)
     data_read = pd.read_csv("./bookrecommender/new_dataset.csv")
@@ -42,9 +16,6 @@ def recommend():
         sort_books, data_read.sort_values(by=["rating-avg"], ascending=False)
     )
     df = pd.DataFrame(merge_books)
-    # df.style.format(BookTable.make_image)
-    # output_dict = df.to_dict(orient='records')
-    # table = BookTable(output_dict)
     return render_template(
         "test.html",
         df=df.to_html(),
@@ -52,13 +23,3 @@ def recommend():
         column=df.columns.values,
         row_data=list(df.values.tolist()),
     )
-
-
-# @app.route("/ok", methods=["POST", "GET"])
-# def ok():
-#     data_read = pd.read_csv("./bookrecommender/test2.csv")
-#     df = pd.DataFrame(data_read)
-#     df.style.format(BookTable)
-#     output_dict = df.to_dict(orient="records")
-#     table = BookTable(output_dict)
-#     return render_template("ok.html", table=table.__html__())
