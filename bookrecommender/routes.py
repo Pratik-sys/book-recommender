c85a27de75ccbeb684fc05216c4b41dace67b368
Pratik-Sys <pithore.pratik@gmail.com>
2020-10-21 10:57:42 +0530

    modify the homw route
    - as we are using the dropdown so there is no need of the search bar and becasue of that the forms are unusable.
    
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -2,16 +2,21 @@ from bookrecommender import app
 from flask import render_template, url_for, request
 import pandas as pd
 import numpy as np
-from bookrecommender.forms import GetField
+# from bookrecommender.forms import GetField
 
 
 @app.route("/home", methods=["POST", "GET"])
 @app.route("/", methods=["POST", "GET"])
 def home():
 
-    form = GetField(request.form)
+    # form = GetField(request.form)
+    if request.method == "POST":
+        h = request.form["catdrop"]
+    else:
+        pass
     data_read = pd.read_csv("./bookrecommender/new_dataset.csv")
-    sort_books = pd.DataFrame([form.title.data], columns=["Tags"])
+    new = pd.DataFrame(data_read)
+    sort_books = pd.DataFrame([h], columns=["Tags"])
     merge_books = pd.merge(
         sort_books, data_read.sort_values(by=["rating-avg"], ascending=False)
     )
@@ -19,7 +24,8 @@ def home():
     return render_template(
         "home.html",
         df=df.to_html(),
-        form=form,
+        # form=form,
         column=df.columns.values,
         row_data=list(df.values.tolist()),
+        new_row = list(new.values.tolist())
     )
