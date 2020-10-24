3a00c46ba3ebb20a6cd69ce6f0d4b6aaf9badfc5
Pratik-Sys <pithore.pratik@gmail.com>
2020-10-24 17:12:12 +0530

    Fix LOCAL_UNBOUND_ERROR
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -1,19 +1,19 @@
 from bookrecommender import app
 from flask import render_template, url_for, request
 import pandas as pd
-import numpy as np
+
 # from bookrecommender.forms import GetField
+
+DEFAULT_H = "Python Programming"
 
 
 @app.route("/home", methods=["POST", "GET"])
 @app.route("/", methods=["POST", "GET"])
 def home():
-
-    # form = GetField(request.form)
     if request.method == "POST":
         h = request.form["catdrop"]
     else:
-        pass
+        h = DEFAULT_H
     data_read = pd.read_csv("./bookrecommender/new_dataset.csv")
     new = pd.DataFrame(data_read)
     sort_books = pd.DataFrame([h], columns=["Tags"])
@@ -21,11 +21,10 @@ def home():
         sort_books, data_read.sort_values(by=["rating-avg"], ascending=False)
     )
     df = pd.DataFrame(merge_books)
+
     return render_template(
         "home.html",
-        df=df.to_html(),
-        # form=form,
-        column=df.columns.values,
         row_data=list(df.values.tolist()),
-        new_row = list(new.values.tolist())
+        new_row=list(new.values.tolist()),
     )
+
