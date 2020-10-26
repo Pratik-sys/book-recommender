ff7080731bd053ed70111c5fcf7a73d8711ea4da
Pratik-Sys <pithore.pratik@gmail.com>
2020-10-26 14:14:38 +0530

    Remove unwanted list
    Signed-off-by: Pratik-Sys <pithore.pratik@gmail.com>

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -15,16 +15,11 @@ def home():
     else:
         h = DEFAULT_H
     data_read = pd.read_csv("./bookrecommender/new.csv")
-    new = pd.DataFrame(data_read)
     sort_books = pd.DataFrame([h], columns=["Tags"])
     merge_books = pd.merge(
         sort_books, data_read.sort_values(by=["rating-avg"], ascending=False)
     )
     df = pd.DataFrame(merge_books)
 
-    return render_template(
-        "home.html",
-        row_data=list(df.values.tolist()),
-        new_row=list(new.values.tolist()),
-    )
+    return render_template("home.html",row_data=list(df.values.tolist()))
 
