653a4766a5886b5ea7a6d9b00ba76fc18fe2b08c
Pratik-sys <pithore.pratik@gmail.com>
2021-10-11 13:24:16 +0530

    clean all useless code

--- bookrecommender/forms.py
+++
@@ -1,5 +0,0 @@
-from wtforms import Form, StringField, SubmitField, TextField, validators
-
-class GetField(Form):
-    title = TextField('Title', [validators.data_required()])
-    submit = SubmitField('search')
--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -1,7 +1,6 @@
 from bookrecommender import app
-from flask import render_template, url_for, request
+from flask import render_template,request
 import pandas as pd
-from bookrecommender.forms import GetField
 
 DEFAULT_H = "Python Programming"
 DEFAULT_TITLE = ""
@@ -18,29 +17,4 @@ def home():
     df = pd.DataFrame(merge_books)
 
     return render_template("home.html",row_data=list(df.values.tolist()))
-
-@app.route("/display", methods=["POST", "GET"])
-def display():
-    return render_template('display.html')
-
-@app.route("/test", methods=["POST", "GET"])
-def test():
-    return render_template('test.html')
-    
-@app.route("/search", methods=["POST", "GET"])
-def search():
-    if request.method == 'POST':
-        title = request.form["books"]
-    else:
-        title = DEFAULT_TITLE
-
-    data_read = pd.read_csv("./bookrecommender/Dataset.csv")
-    title_sort = pd.DataFrame([title], columns=["Title"])
-    merge_title = pd.merge(title_sort, data_read.sort_values(by=["rating-avg"], ascending=False))
-    publisher_sort = pd.DataFrame([title], columns=["Publishers"])
-    merge_publisher = pd.merge(publisher_sort, data_read.sort_values(by=["rating-avg"], ascending=False))
-    title_df = pd.DataFrame(merge_title)
-    publish_df = pd.DataFrame(merge_publisher)
-
-    return render_template('search.html', title=list(title_df.values.tolist()),publish=list(publish_df.values.tolist()))
     
