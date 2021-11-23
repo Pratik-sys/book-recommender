af3d7e9c2c730b8bf7aff5cea346d837a1af4c7b
Pratik-sys <pithore.pratik@gmail.com>
2021-11-23 12:12:07 +0530

    use ternary condition in dorpdown menu

--- bookrecommender/routes.py
+++ bookrecommender/routes.py
@@ -2,19 +2,15 @@ from bookrecommender import app
 from flask import render_template,request
 import pandas as pd
 
-DEFAULT_H = "Python Programming"
-DEFAULT_TITLE = ""
-@app.route("/home", methods=["POST", "GET"])
 @app.route("/", methods=["POST", "GET"])
 def home():
+    section=""
     data_read = pd.read_csv("./bookrecommender/Dataset.csv")
     if request.method == "POST":
-        h = request.form["catdrop"].lower()
-    else:
-        h = DEFAULT_H
-    sort_books = pd.DataFrame([h], columns=["Tags"])
+        section = request.form["catdrop"].lower()
+    sort_books = pd.DataFrame([section], columns=["Tags"])
     merge_books = pd.merge(sort_books, data_read.sort_values(by=["rating-avg"], ascending=False))
     df = pd.DataFrame(merge_books)
 
-    return render_template("home.html",h=h,row_data=list(df.values.tolist()))
+    return render_template("home.html",section=section,row_data=list(df.values.tolist()))
     
